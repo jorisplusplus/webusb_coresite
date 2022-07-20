1e0e78346a16763321b4e83bc38e4a91037a43ad
Joris Witteman <joriswitteman@hotmail.com>
2022-07-20 20:49:46 +0200

    Added buttons and ace

--- src/components/ProgrammingView.vue
+++ src/components/ProgrammingView.vue
@@ -1,61 +1,101 @@
 <script setup>
-  import { ref, reactive} from 'vue';
-  import Tree from "vue3-treeview";
-  import "vue3-treeview/dist/style.css";
+import { ref, reactive } from "vue";
+import Tree from "vue3-treeview";
+import "vue3-treeview/dist/style.css";
+import { VAceEditor } from "vue3-ace-editor";
 
+const config = reactive({
+  roots: ["id1", "id2"],
+  leaves: [],
+});
 
-  const config = reactive({
-        roots: ["id1", "id2"],
-        leaves: [],
-      })
-    
-  const nodes = reactive({
-        id1: {
-          text: "text1",
-          children: ["id11", "id12"],
-        },
-        id11: {
-          text: "text11",
-        },
-        id12: {
-          text: "text12",
-        },
-        id2: {
-          text: "text2",
-        },
-      })
-  
-  function addServerNode(n) {
-      console.log("tata");
+const filename = ref("/flash/cache/scratch.py")
 
-      if (n.children && n.children.length > 0) return;
+const nodes = reactive({
+  id1: {
+    text: "text1",
+    children: ["id11", "id12"],
+  },
+  id11: {
+    text: "text11",
+  },
+  id12: {
+    text: "text12",
+  },
+  id2: {
+    text: "text2",
+  },
+});
 
-      // set node loading state to tree
-      n.state.isLoading = true;
+function addServerNode(n) {
+  console.log("tata");
 
-      // fake server call
-      setTimeout(() => {
-        // create a fake node
-        const id = `${Date.now()}`;
-        const newNode = {
-          text: `loaded from server`,
-          children: false,
-          state: {},
-        };
+  if (n.children && n.children.length > 0) return;
 
-        // add the node to nodes
-        nodes[id] = newNode;
-        config.leaves.push(id);
-        // set children
-        n.children = [id];
-        // end loading
-        n.state.isLoading = false;
-      }, 500);
-    }
+  // set node loading state to tree
+  n.state.isLoading = true;
+
+  // fake server call
+  setTimeout(() => {
+    // create a fake node
+    const id = `${Date.now()}`;
+    const newNode = {
+      text: `loaded from server`,
+      children: false,
+      state: {},
+    };
+
+    // add the node to nodes
+    nodes[id] = newNode;
+    config.leaves.push(id);
+    // set children
+    n.children = [id];
+    // end loading
+    n.state.isLoading = false;
+  }, 500);
+}
 </script>
 
 <template>
     <v-card class="ma-3 pa-4">
+      <v-row no-gutters>
+        <v-col cols="3">
+          <v-row no-gutters>
+          <v-btn color="primary" class="grey lighten-4 ma-1">
+            <v-icon large>mdi-folder-plus</v-icon>
+          </v-btn>
+          <v-btn color="primary" class="grey lighten-4 ma-1">
+            <v-icon large>mdi-file</v-icon>
+          </v-btn>
+          <v-btn color="primary" class="grey lighten-4 ma-1">
+            <v-icon large>mdi-file-document</v-icon>
+          </v-btn>
+          <v-btn color="primary" class="grey lighten-4 ma-1">
+            <v-icon large>mdi-delete</v-icon>
+          </v-btn>
+          <v-btn color="primary" class="grey lighten-4 ma-1">
+            <v-icon large>mdi-download</v-icon>
+          </v-btn>
+          </v-row>
+        </v-col>
+        <v-col>
+          <v-row no-gutters>
+          <v-btn color="primary" class="grey lighten-4 ma-1">
+            <v-icon large>mdi-play</v-icon>
+          </v-btn>
+          <v-btn color="primary" class="grey lighten-4 ma-1">
+            <v-icon large>mdi-content-save</v-icon>
+          </v-btn>
+          <v-text-field
+            class="mt-n3"
+            label="Filename"
+            v-model="filename"
+          ></v-text-field>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row no-gutters>
+       <v-col cols="3">
       <Tree :nodes="nodes" :config="config" @nodeOpened="addServerNode">
         <template #loading-slot>
           <div class="progress">
@@ -63,6 +103,15 @@
           </div>
         </template>
       </tree>
+      </v-col>
+      <v-col>
+      <v-ace-editor
+        lang="html"
+        value="test"
+        theme="chrome"
+        style="height: 600px" />
+    </v-col>
+    </v-row>
     </v-card>
     <v-card class="ma-3 pa-4">
         test
@@ -70,8 +119,15 @@
 </template>
 
 <style scoped>
-* {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    font-size: 14px;
+/* * {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-size: 14px;
+} */
+.v-btn {
+  min-width: 35px;
+  width:35px;
+  min-height: 35px;
+  height: 35px;
 }
 </style>
